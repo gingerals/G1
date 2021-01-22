cc73b3dcc3a7efa53b116bd2106b95469f1aa0c2
gingerals <68650398+gingerals@users.noreply.github.com>
2021-01-22 11:21:46 -0600

    Update sintetizador.py

--- sintetizador.py
+++ sintetizador.py
@@ -1,68 +1,29 @@
-# Librerias: -----------------------------------------------
 
+# Interfaz gráfica para el sintetizador
+
+
+# Libreria para generar gráficos
+from tkinter import *
+from tkinter.font import Font
 # Libreria numpy para generar los valores de la funcion seno
 import numpy as np 
 # Libreria para reproducir la nota
 import sounddevice as sd
 
 
-# Parametros: ----------------------------------------------
+
+# Parametros: ----------------------------------------------------
 
 # Tasa de muestreo (sampling rate)
 muestreo = 44100
 
 # Duración del sonido a generar en SEGUNDOS
-duracion_s = 5.0
+duracion_s = 1.0
 
 
 
-# Funciones: ------------------------------------------------
 
-# Instrucciones: 
-def instrucciones():
-    print ("Ingrese que nota quiere tocar")
-    print ("1.   B4 ")
-    print ("2.   As4")
-    print ("3.   A4 ")
-    print ("4.   Gs4")
-    print ("5.   G4 ")
-    print ("6.   Fs4")
-    print ("7.   F4 ")
-    print ("8.   E4 ")
-    print ("9.   Ds4")
-    print ("10.   D4 ")
-    print ("11.   Cs4")
-    print ("12.   C4 ")
-    print ("-Presione 0 para salir-")
-
-
-
-# Función switch para obtener el sonido cada nota Hz
-def notasHz():
-
-    seleccion = int(input('Selección: ')) # Seleccion
-
-
-    switch ={ # frecuencia para cada nota
-        1: '493.883',
-        2: '466.164',
-        3: '440.000',
-        4: '415.305',
-        5: '391.995',
-        6: '369.994',
-        7: '349.228',
-        8: '329.628',
-        9: '311.127',
-        10: '293.665',
-        11: '277.183',
-        12: '261.626'
-    }
-
-    def default(): #
-        exit()
-
-    return switch.get(seleccion, default)
-
+# Funciones: -----------------------------------------------------
 
 # Metodo para crear la onda dado determinada frecuencia y duración
 def sintetizar( frecHz ):
@@ -74,7 +35,7 @@ def sintetizar( frecHz ):
     # La funcion SIN retorna el seno de cada X dado, al ingresar un arreglo de muestras multiplicado por 
     # el valor de un ciclo completo y la frecuencia nos regresara las cordenadas de todos los puntos y de cada muestra
     onda = np.sin(2 * np.pi * muestras * frecHz / muestreo)
-
+    
     # Se tiene que atenuar la onda ya que los valores en y ocupan toda la profundidad del bit rate o 
     # la amplitud, es decir el volumen máximo
     ondaTenue = onda * 0.3
@@ -82,18 +43,140 @@ def sintetizar( frecHz ):
     # Obtenemos los valores en enteros validos para el formato de audio estándard
     ondaEnteros = np.int16(ondaTenue * 32767)
 
+    # FUncion para reproducir, esperar y detener el sonido
     sd.play(ondaEnteros, muestreo)
     sd.wait(duracion_s)
     sd.stop()
 
 
 
-# Loop:-----------------------------------------------------
+# Función switch para obtener el sonido cada nota Hz
+def notasHz( nota ): # recibe como parametro la NOTA escogida
 
-# se repetira hasta que el usuario ingrese algun valor no valido
-loop = 1
-while loop != 0 :
+    switch ={ # frecuencia para cada nota
+        1:  '987.7666', #B5
+        2:  '932.3275', #As5
+        3:  '880.0000', #A5
+        4:  '830.6094', #Gs5
+        5:  '783.9909', #G5
+        6:  '739.9888', #Fs5
+        7:  '698.4565', #F5
+        8:  '659.2551', #E5
+        9:  '622.2540', #Ds5
+        10: '587.3295', #D5
+        11: '554.3653', #Cs5
+        12: '523.2511', #C5
+    }
+    def default(): # En caso de no recibir un valor incorrecto
+        exit()
 
-    instrucciones()
+    return switch.get(nota, default) # La función regresa la frecuencia de la nota tocada
+    
 
-    sintetizar( float( notasHz() ) )
+
+# Función para obtener la frecuencia y sintetizar el sonido
+def tecla( nota ):
+    sintetizar( float( notasHz( nota ) ) )
+
+
+
+
+
+
+    
+    
+# Ventana: -------------------------------------------------------
+ventana = Tk() # Se instancia la ventana 
+ventana.geometry("1000x500") # Dimensiones
+ventana.title("Sintetizador G1") # Titulo
+# Marco
+marco = Frame(ventana)
+marco.pack()
+# Texto 
+titulo  = Label (ventana, text = "S I N T E T I Z A D O R  \n <- G 1 ->", font=('Arcade Interlaced', 25), fg='#ff8080') 
+#estilo = Font(ventana, family=, size=25, )
+titulo.pack(padx=10, pady=10) # Alinear 
+
+# teclas del piano: .................................................................
+# Margen teclas negras: .................................................................
+mSuperior = Frame(ventana) # Marco superior
+mSuperior.pack(side = TOP) # Alinearse abajo
+
+# teclas negras: .................................................................
+#Do#/Reb
+botonCs = Button(mSuperior, padx=8, height=6, pady=8, bd=8, text="C#", bg="black", fg="white", command=lambda: tecla(11))
+botonCs.pack(side = LEFT)
+
+#espacio
+espacio = Button(mSuperior, state=DISABLED, height=7, width=1, padx=0, pady=0, relief=RIDGE)
+espacio.pack(side=LEFT)
+
+#Re#/Mib
+botonDs = Button(mSuperior, padx=8, height=6, pady=8, bd=8, text="D#", bg="black", fg="white", command=lambda: tecla(9))
+botonDs.pack(side = LEFT)
+
+#espacio
+espacio = Button(mSuperior, state=DISABLED, height=7, width=4, padx=0, pady=0, relief=RIDGE)
+espacio.pack(side=LEFT)
+
+#Fa#/Solb
+botonFs = Button(mSuperior, padx=8, height=6, pady=8, bd=8, text="F#", bg="black", fg="white", command=lambda: tecla(6))
+botonFs.pack(side = LEFT)
+
+#espacio
+espacio = Button(mSuperior, state=DISABLED, height=7, width=1, padx=0, pady=0, relief=RIDGE)
+espacio.pack(side=LEFT)
+
+#Sol#/Lab
+botonGs = Button(mSuperior, padx=8, height=6, pady=8, bd=8, text="G#", bg="black", fg="white", command=lambda: tecla(4))
+botonGs.pack(side = LEFT)
+
+#espacio
+espacio = Button(mSuperior, state=DISABLED, height=7, width=1, padx=0, pady=0, relief=RIDGE)
+espacio.pack(side=LEFT)
+
+#La#/Sib
+botonAs = Button(mSuperior, padx=8, height=6, pady=8, bd=8, text="A#", bg="black", fg="white", command=lambda: tecla(2))
+botonAs.pack(side = LEFT)
+
+
+
+# # Margen teclas blancas: .................................................................
+mInferior = Frame(ventana) # Marco inferior (piano)
+mInferior.pack(side = TOP) # Alinearse abajo
+
+# teclas blancas: ...........................................................................
+#Do
+botonC = Button(mInferior, padx=16, height=8, pady=16, bd=8, text="C", bg="white", fg="black", command=lambda: tecla(12))
+botonC.pack(side = LEFT)
+
+#Re
+botonD = Button(mInferior, padx=16, height=8, pady=16, bd=8, text="D", bg="white", fg="black", command=lambda: tecla(10))
+botonD.pack(side = LEFT)
+
+#Mi
+botonE = Button(mInferior, padx=16, height=8, pady=16, bd=8, text="E", bg="white", fg="black", command=lambda: tecla(8))
+botonE.pack(side = LEFT)
+
+#Fa
+botonF = Button(mInferior, padx=16, height=8, pady=16, bd=8, text="F", bg="white", fg="black", command=lambda: tecla(7))
+botonF.pack(side = LEFT)
+
+#Sol
+botonG = Button(mInferior, padx=16, height=8, pady=16, bd=8, text="G", bg="white", fg="black", command=lambda: tecla(5))
+botonG.pack(side = LEFT)
+
+#La
+botonA = Button(mInferior, padx=16, height=8, pady=16, bd=8, text="A", bg="white", fg="black", command=lambda: tecla(3))
+botonA.pack(side = LEFT)
+
+#Si
+botonB = Button(mInferior, padx=16, height=8, pady=16, bd=8, text="B", bg="white", fg="black", command=lambda: tecla(1))
+botonB.pack(side = LEFT)
+
+
+# Se crea la ventana: .........................................................................
+# Agregamos icono
+ico = PhotoImage(file='/audio-waves.png')
+ventana.iconphoto(False, ico)
+ventana.mainloop() # Crear el hilo o bucle que va a recibir los eventos
